ce0f8b2fab719dd28c8d153e9520f6a233ba1ea3
ak2hr <ak2hr@virginia.edu>
2019-04-10 15:22:59 -0400

    added login/logout feature with cookies

---
+++ logout.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="authors" content="Trevor Bedsaul and Andrew Kepley">
+    <title>Login</title>
+
+    <!--bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="styles/login.css" />
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="home.html">CrushRush</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li>
+            <li class="nav-item"><a class="nav-link" href="home.php">Register</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Search</a></li>
+        </ul>                
+    </div>
+    <span class="navbar-text">
+        <a class="navbar-brand" href="login.php">Login</a>
+    </span>
+</nav>
+
+<div>
+	You have been logged out
+</div>
+
+<?php
+	setcookie('user', $user, time()-3600);
+?>
+
+</body>
+
+</html>
--- search.php
+++ search.php
@@ -32,11 +32,18 @@
         <a class="navbar-brand" href="home.html">CrushRush</a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link" href="profile.html">Profile</a></li>
-                <li class="nav-item"><a class="nav-link" href="home.html">Register</a></li>
+                <li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li>
+                <li class="nav-item"><a class="nav-link" href="home.php">Register</a></li>
                 <li class="nav-item active"><a class="nav-link" href="#">Search</a></li>
-            </ul>    
+            </ul>                
         </div>
+        <span class="navbar-text">
+            <?php if(isset($_COOKIE['user'])) : echo $_COOKIE['user'];?>
+                <a class="navbar-brand" href="logout.php">, Logout</a>
+            <?php else : ?>
+                <a class="navbar-brand" href="login.php">Login</a>
+            <?php endif; ?>        
+        </span>
     </nav>
 
     <div id="searchContainer">
